1330751568c223470b69d52f942c10c6151004a5
DT5Gen <marinovod@gmail.com>
2022-03-18 03:25:59 +0300

    L2 2 добавили поддержку viewBinding

--- app/src/main/java/com/gb/kotlin_1728_2_1/view/MainActivity.kt
+++ app/src/main/java/com/gb/kotlin_1728_2_1/view/MainActivity.kt
@@ -1,87 +1,18 @@
 package com.gb.kotlin_1728_2_1.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import com.gb.kotlin_1728_2_1.lesson1_reservation.NoteKotlin
-import com.gb.kotlin_1728_2_1.lesson1_reservation.Person
+import androidx.appcompat.app.AppCompatActivity
 import com.gb.kotlin_1728_2_1.R
-import com.gb.kotlin_1728_2_1.lesson1_reservation.Repository
+import com.gb.kotlin_1728_2_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO создать проект в гит
-        setContentView(R.layout.activity_main)
-        val person = Person("noInit", 32)
-        person.newProperty = ""
-        Log.d("TAG", person.newProperty)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Repository.getData()
-        val callback = object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                TODO("Not yet implemented")
-            }
-
-        }
-
-        Person.test
-        Person.testPublicStaticFinal
-        Person.testCompan()
-
-        var result = if (false) {
-            1
-        } else {
-            2
-        }
-        Log.d("TAG", "$result")
-
-        result = when (WeatherType.CLOUDY) {
-            WeatherType.SUNNY -> 1
-            WeatherType.RAINY -> 2
-            WeatherType.CLOUDY -> 3
-        }
-
-        Log.d("TAG", "$result")
-
-        for (i in 0..20 step 2) {
-            Log.d("TAG", "$i")
-        }
-        for (i in 0 until 20 step 5) {
-            Log.d("TAG", "$i")
-        }
-        for (i in 20 downTo 0 step 2) {
-            Log.d("TAG", "$i")
-        }
-
-        repeat(6) {
-            Log.d("TAG", "${it + 1}")
-        }
-
-        val dayzOfWeek =
-            listOf("sunday", "monday", "Tuesday", "Wednesday", "Thyrsday", "Friday", "Saturday")
-        dayzOfWeek.forEach{
-            Log.d("TAG", it)
-        }
-        for(day in dayzOfWeek) {
-            Log.d("TAG", day)
-        }
-
-        val cat = object {
-            val name = "Kisa"
-            val age = 3
-        }
-        Log.d("CAT", " Cat name ${cat.name} older ${cat.age}")
-    }
-
-    val notes = NoteKotlin("", 1)
-    val copyNote = notes.copy("New")
-
-    enum class WeatherType {
-        SUNNY,
-        RAINY,
-        CLOUDY
     }
 
 }
