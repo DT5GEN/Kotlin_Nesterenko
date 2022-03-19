dd19ac259a6b3caac781103db430628129e13f22
DT5Gen <marinovod@gmail.com>
2022-03-20 02:10:44 +0300

    L3 3 повторили маски на дженериках

--- app/src/main/java/com/gb/kotlin_1728_2_1/lesson3/Car.kt
+++ app/src/main/java/com/gb/kotlin_1728_2_1/lesson3/Car.kt
@@ -1,12 +1,10 @@
 package com.gb.kotlin_1728_2_1.lesson3
 
-class Car (val name:String, val age:Int) : Any() , FlyingVehicle{
+import android.util.Log
+import android.view.ViewGroup
 
+class Car(val name: String, val age: Int) : Any(), FlyingVehicle {
 
-
-    fun main(): Unit {
-
-    }
 
     override val greeting: String
         get() {
@@ -20,5 +18,36 @@ class Car (val name:String, val age:Int) : Any() , FlyingVehicle{
 
     override fun speed(weight: Float): Float {
         return maxSpeed
+    }
+
+
+    fun main(): Unit {
+        writeInt(1)
+        writeDouble(1.0)
+        writeString("1")
+
+
+        write(1)
+        write(1.0)
+        write("1")
+    }
+    fun writeInt(input:Int) = Log.d("mylog", input.toString())
+    fun writeString(input:String) = Log.d("mylog", input.toString())
+    fun writeDouble(input:Double) = Log.d("mylog", input.toString())
+
+
+    fun <T> write(input:T) = Log.d("mylog", input.toString())
+    fun <T:Any> write2(input:T) = Log.d("mylog", input.toString())
+    // <T> и <T:Any> в данной ситуации принимают все типы
+    fun <T:ViewGroup> write(input:T) = Log.d("mylog", input.toString())
+    // дженерик принимает ограниченные  ViewGroup тип
+
+
+}
+
+class Producer<T>{
+    private val  hack = mutableListOf<T>()
+    fun produce():T{
+        return hack.last()
     }
 }
