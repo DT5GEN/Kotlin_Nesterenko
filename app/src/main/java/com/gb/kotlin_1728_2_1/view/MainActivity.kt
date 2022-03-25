359fda10955a49e682b86751fb8492ffdbf9a466
DT5Gen <marinovod@gmail.com>
2022-03-25 16:52:50 +0300

    L6 протестировал вспомогательный поток и  результат перевели в MAIN (UI, главный) поток

--- app/src/main/java/com/gb/kotlin_1728_2_1/view/MainActivity.kt
+++ app/src/main/java/com/gb/kotlin_1728_2_1/view/MainActivity.kt
@@ -2,10 +2,13 @@ package com.gb.kotlin_1728_2_1.view
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.gb.kotlin_1728_2_1.R
 import com.gb.kotlin_1728_2_1.databinding.ActivityMainBinding
 import com.gb.kotlin_1728_2_1.lesson4.main
+import com.gb.kotlin_1728_2_1.lesson6.ThreadsFragment
 import com.gb.kotlin_1728_2_1.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +24,25 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance()).commit()
         }
-            // main()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+                     R.id.menu_threads->{
+                         supportFragmentManager.beginTransaction()
+                             .add(R.id.container, ThreadsFragment.newInstance())
+                             .addToBackStack("").commit()
+            true
+        } else ->{
+             super.onOptionsItemSelected(item)
+         }
+
+        }
     }
 }
 
